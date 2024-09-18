41bef248044d7cdc94924b3220dc5faeab6ec37c
WowDaBug <110950607+WowDaBug@users.noreply.github.com>
2024-09-18 18:03:03 -0500

    Delete eldreffup directoryerer

--- eldreffup/src/Utils/RedToGreenLerpColor.js
+++
@@ -1,6 +0,0 @@
-export default function getRedtoGreenLerpColor(power){
-    return `rgb(
-        ${Math.floor((power <= 0.5) ? 255 : ((1-power) * 2 * 255))}, 
-        ${Math.floor(Math.min(255, power * 2 * 255))}, 
-        0)`
-}
